74aa4b615fcad365799678b7f22c56e1e737d327
Danilo Paulino da Silva <danilopaulinodasilva@gmail.com>
2020-11-22 08:05:31 -0300

    reverted to crud

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,7 @@ import { Router } from 'express';
 const routes = Router();
 
 import userRoutes from './user/index.js';
-import movieRoutes from './movie/index.js';
 
 routes.use("/user", userRoutes);
-routes.use("/movie", movieRoutes);
 
 export default routes;
--- src/routes/movie/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-const routes = Router();
-
-routes.get('/', (req,res) => {
-    res.send("movies route");
-});
-
-export default routes;
